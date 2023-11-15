8608942ef0003b42374bc7281fac8b2fd1b20581
Emmanuel Áviles Pacheco <eavilesp@aeromexico.com>
2023-11-15 10:48:44 -0600

    base de datos

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,6 +1,8 @@
+
 module.exports = ({ env }) => ({
-  url: env("RENDER_EXTERNAL_URL"),
-  dirs: {
-    public: "/data/public"
+  proxy: true,
+  url: env('APP_URL'), // Sets the public URL of the application.
+  app: { 
+    keys: env.array('APP_KEYS')
   },
 });
